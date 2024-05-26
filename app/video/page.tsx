9a3e1e3b99706faabdcb40e24a47ccd5c46d87cc
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-05-27 00:30:22 +0900

    chore: change code for debug

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,31 +1,43 @@
 import { Suspense } from "react";
-import { getVideoByComponents } from "../actions";
 import SlideListWrapper from "@/components/gallery/SlideListWrapper";
 
 export default async function VideoPage() {
   const data = await getVideoByComponents();
-  console.log(data);
-
-  if (data.length > 0) {
-    return <></>;
-  }
 
   return (
     <section className="prose dark:prose-invert max-w-none flex-1 flex flex-col">
       <h1>Videos</h1>
 
       <Suspense>
-        <SlideListWrapper>
-          {data.map((item) => (
-            <li
-              className="relative flex flex-col shadow-lg rounded-md p-0 pb-[56.25%] border overflow-hidden bg-black"
-              key={item.key}
-            >
-              {item}
-            </li>
-          ))}
-        </SlideListWrapper>
+        <SlideListWrapper>{data}</SlideListWrapper>
       </Suspense>
     </section>
   );
+}
+
+export async function getVideoByComponents() {
+  const YOUTUBE = [
+    { id: 0, key: "RyBy_mB4Ex4" },
+    { id: 1, key: "C5cSWuhuU1I" },
+    { id: 2, key: "0F3xBCxkDXI" },
+    { id: 3, key: "CaybAzvcRGo" },
+    { id: 4, key: "UF6L6QfTCdo" },
+    { id: 5, key: "Vb1GvLyru2Q" },
+    { id: 6, key: "jOs3qfMGhNI" },
+  ];
+
+  return YOUTUBE.map((video) => (
+    <li
+      className="relative flex flex-col shadow-lg rounded-md p-0 pb-[56.25%] border overflow-hidden bg-black"
+      key={video.key}
+    >
+      <iframe
+        key={video.key}
+        src={`https://www.youtube.com/embed/${video.key}`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="absolute w-full h-full scale-[101%] -top-5 left-0"
+      />
+    </li>
+  ));
 }
