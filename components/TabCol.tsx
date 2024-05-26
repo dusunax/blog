f37161923b9943aae4753a5a18b752af6e88b9fc
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-05-26 21:34:46 +0900

    feat: responsive tab buttons

--- components/TabCol.tsx
+++ components/TabCol.tsx
@@ -26,7 +26,7 @@ const TabCol = ({ tabIndex, setTabIndex }: Props) => {
   ];
 
   return (
-    <ul className="flex flex-col absolute left-0 -translate-x-full pl-4 gap-y-2">
+    <ul className="flex flex-row lg:flex-col absolute pl-4 my-0 lg:-translate-x-full -translate-y-full right-0 lg:right-auto lg:left-0 lg:top-0 lg:mt-4 lg:translate-y-0 gap-y-2 ">
       {TABS.map((tab, index) => (
         <Tag
           {...tab}
@@ -47,7 +47,7 @@ interface TagProps extends Pick<Props, "tabIndex" | "setTabIndex">, Tab {
 function Tag({ name, index, bg, color, tabIndex, setTabIndex }: TagProps) {
   return (
     <ol
-      className={`my-0 pl-4 pr-3 py-1 bg-black text-right font-semibold text-white text-sm rounded-l cursor-pointer transition-opacity ${
+      className={`my-0 pl-4 pr-3 py-1 bg-black text-right font-semibold text-white text-sm rounded-t lg:rounded-r-none lg:rounded-l cursor-pointer transition-opacity ${
         tabIndex !== index ? "opacity-50" : ""
       }`}
       style={{ backgroundColor: bg, color: color }}
